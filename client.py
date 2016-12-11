16607326b76144177a7a65b0b59d577c9501b9b7
John Gerace <johngerace@gmail.com>
2016-12-11 18:09:51 -0500

    Initial commit

---
+++ client.py
@@ -0,0 +1,209 @@
+import argparse
+from datetime import date, datetime, time, timedelta
+import functools
+import json
+import threading
+import time as timemod
+
+from tornado import escape
+from tornado import gen
+from tornado import httpclient
+from tornado import httputil
+from tornado import ioloop
+from tornado import websocket
+
+json_encoder = lambda obj: obj.isoformat() if isinstance(obj, time) else None
+
+
+APPLICATION_JSON = 'application/json'
+
+DEFAULT_CONNECT_TIMEOUT = 60
+DEFAULT_REQUEST_TIMEOUT = 60
+
+
+class WebSocketClient():
+    """Base for web socket clients.
+    """
+ 
+    def __init__(self, *, connect_timeout=DEFAULT_CONNECT_TIMEOUT,
+                 request_timeout=DEFAULT_REQUEST_TIMEOUT):
+
+        self.connect_timeout = connect_timeout
+        self.request_timeout = request_timeout
+
+    def connect(self, url):
+        """Connect to the server.
+        :param str url: server URL.
+        """
+
+        headers = httputil.HTTPHeaders({'Content-Type': APPLICATION_JSON})
+        request = httpclient.HTTPRequest(url=url,
+                                         connect_timeout=self.connect_timeout,
+                                         request_timeout=self.request_timeout,
+                                         headers=headers)
+        ws_conn = websocket.WebSocketClientConnection(ioloop.IOLoop.current(),
+                                                      request)
+        ws_conn.connect_future.add_done_callback(self._connect_callback)
+
+    def send(self, data):
+        """Send message to the server
+        :param str data: message.
+        """
+        if not self._ws_connection:
+            raise RuntimeError('Web socket connection is closed.')
+
+        self._ws_connection.write_message(escape.utf8(json.dumps(data, default=json_encoder)))
+
+    def close(self):
+        """Close connection.
+        """
+        if not self._ws_connection:
+            raise RuntimeError('Web socket connection is already closed.')
+
+        self._ws_connection.close()
+
+    def _connect_callback(self, future):
+        if future.exception() is None:
+            self._ws_connection = future.result()
+            self._on_connection_success()
+            self._read_messages()
+        else:
+            self._on_connection_error(future.exception())
+
+    @gen.coroutine
+    def _read_messages(self):
+        while True:
+            msg = yield self._ws_connection.read_message()
+            if msg is None:
+                self._on_connection_close()
+                break
+
+            self._on_message(msg)
+
+    def _on_message(self, msg):
+        """This is called when new message is available from the server.
+        :param str msg: server message.
+        """
+        pass
+
+    def _on_connection_success(self):
+        """This is called on successful connection to the server.
+        """
+        pass
+
+    def _on_connection_close(self):
+        """This is called when server closed the connection.
+        """
+        pass
+
+    def _on_connection_error(self, exception):
+        """This is called in case if connection to the server could
+        not established.
+        """
+        pass
+
+
+class TestWebSocketClient(WebSocketClient):
+
+    def _on_message(self, msg):
+        print(msg)
+
+    def _on_connection_success(self):
+        print("connection success!")
+        self.send_status_update()
+
+    def _on_connection_close(self):
+        print('Connection closed!')
+
+    def _on_connection_error(self, exception):
+        print('Connection error: %s', exception)
+
+    def send_status_update(self):
+        global config
+        self.send(json.dumps(config, default=json_encoder))
+
+
+class WebSocketThread(threading.Thread):
+    def __init__(self, url):
+        threading.Thread.__init__(self)
+        self.url = url
+
+    def run(self):
+        global config
+        
+        connectStr = "%s?id=%s" % (self.url, config.get('id'))
+        deviceclient.connect(connectStr)
+
+        try:
+            ioloop.IOLoop.instance().start()
+        except KeyboardInterrupt:
+            deviceclient.close()
+
+
+class WateringThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        return
+        while True:
+            """
+            if config["start"] <= datetime.now().time() <= config["end"]:
+                deviceclient.send_status_update(json.loads({"id": config["id"],
+                                                      "status": "Watering"}))
+            """
+            data = {
+                config["id"]: {
+                    "id": config["id"],
+                    "status": "Ready"
+                }
+            }
+            data["device_1"]["status"] = "Watering"
+            deviceclient.send_status_update(data)
+            timemod.sleep(3)
+            data["device_1"]["status"] = "Ready"
+            deviceclient.send_status_update(data)
+            break
+            
+
+config = {}
+deviceclient = TestWebSocketClient()
+
+def updateConfig(config_path):
+    global config
+
+    with open(config_path, 'r') as fin:
+        data = fin.read()
+        config = json.loads(data)
+
+    if not config.get('id'):
+        raise RuntimeError('Missing id string')
+
+    if config.get('start') and config.get('duration'):
+        start = datetime.strptime(config['start'], '%H:%M:%S')
+        config['end'] = (start + timedelta(seconds=config['duration'])).time()
+
+    config['status'] = 'Ready'
+
+        
+def main(args):
+    updateConfig(args.config)
+
+    url = "ws://localhost:8888/devicews"
+    
+    thread1 = WebSocketThread(url)
+    thread2 = WateringThread()
+
+    thread1.start()
+    thread2.start()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Client plant watering device")
+    parser.add_argument("--config",
+                        type=str,
+                        help="Path to config file",
+                        default="config.cfg")
+    args = parser.parse_args()
+    
+    main(args)
---
+++ server.py
@@ -0,0 +1,149 @@
+from datetime import time
+import os.path
+
+from tornado import ioloop, web, websocket
+from tornado.web import url
+import json
+json_encoder = lambda obj: obj.isoformat() if isinstance(obj, time) else None
+
+deviceSockets = {}
+admins = []
+devices = {}
+
+
+class AdminSocketHandler(websocket.WebSocketHandler):
+    
+    def check_origin(self, origin):
+        return True
+
+    def open(self, *args, **kwargs):
+        print("admin connection")
+        admins.append(self)
+        payload = [{"action": "ADDUP",
+                    "data": devices[device_id]} for device_id in devices]
+        self.write_message(json.dumps(payload, default=json_encoder))
+
+    def on_close(self):
+        if self in admins:
+            print("Admin connection closed!")
+            admins.remove(self)
+
+    def on_message(self, message):
+        data = json.loads(json.loads(message))  # Wat.
+        device_id = data.get('id')
+
+        if device_id:
+            if not device_id in devices:
+                devices[device_id] = data
+
+        print("sending message to admins")
+        for a in admins:
+            payload = [{"action": "ADDUP",
+                        "data": devices[device_id]} for device_id in devices]
+            a.write_message(json.dumps(payload, default=json_encoder))
+
+
+class DeviceSocketHandler(websocket.WebSocketHandler):
+    
+    def check_origin(self, origin):
+        return True
+
+    def open(self, *args, **kwargs):
+        self.id = self.get_argument("id")
+        print("device connection", self.id)
+        if self.id and not devices.get("id"):
+            deviceSockets[self.id] = self
+
+    def on_close(self):
+        print("Device connection closed!")
+        del(devices[self.id])
+        del(deviceSockets[self.id])
+
+        payload = [{"action": "DELETE",
+                   "data": {"device_id": self.id}}]
+        for a in admins:
+            a.write_message(json.dumps(payload, default=json_encoder))
+
+    def on_message(self, message):
+        data = json.loads(json.loads(message))  # Wat.
+        device_id = data.get('id')
+
+        if device_id:
+            if not device_id in devices:
+                devices[device_id] = data
+
+        payload = [{"action": "ADDUP",
+                    "data": devices[device_id]}]
+        for a in admins:
+            print(payload)
+            a.write_message(json.dumps(payload, default=json_encoder))
+
+class ApiHandler(web.RequestHandler):
+
+    @web.asynchronous
+    def get(self, *args):
+        print("API get")
+        self.finish()
+        id = self.get_argument("id")
+        value = self.get_argument("value")
+        data = {"id": id, "value" : value}
+        data = json.dumps(data)
+        for c in cl:
+            c.write_message(data)
+
+    @web.asynchronous
+    def post(self):
+        pass
+
+class AdminHandler(web.RequestHandler):
+
+    @web.asynchronous
+    def get(self, *args):
+        self.render("index.html", devices=devices)
+        
+
+class DeviceHandler(web.RequestHandler):
+
+    @web.asynchronous
+    def get(self, *args):
+        self.finish()
+
+        for a in admins:
+            a.write_message(json.dumps(devices))
+
+
+class DeviceProfileHandler(web.RequestHandler):
+
+    @web.asynchronous
+    def get(self, id):
+        self.render("device.html", device=devices[id])
+
+    @web.asynchronous
+    def delete(self, id):
+        self.finish()
+        if id in devices.keys():
+            del(devices[id])
+
+        for a in admins:
+            a.write_message(json.dumps(devices))
+
+handlers = [
+    url(r'/', AdminHandler, name="home"),
+    (r'/adminws', AdminSocketHandler),
+    (r'/devicews', DeviceSocketHandler),
+    (r'/api', ApiHandler),
+    url(r'/device', DeviceHandler),
+    url(r'/device/(.*)', DeviceProfileHandler, name="device"),
+    (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
+    (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
+]
+
+settings = dict(
+    template_path=os.path.join(os.path.dirname(__file__), "templates")
+)
+app = web.Application(handlers, **settings)
+
+if __name__ == '__main__':
+    # TODO: Configure port via params
+    app.listen(8888)
+ioloop.IOLoop.instance().start()
